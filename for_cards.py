c3fe86f317431f2698da74958d7ce3c260b82d56
OlegAndreev <for.studying.sf.oa@gmail.com>
2025-01-13 21:47:22 +0300

    correct cards

--- for_cards.py
+++ for_cards.py
@@ -1,3 +1,4 @@
 import numpy as np
-cards = np.random.choice(range(1, 25), size=4, replace=False)
-print(f'Дима - {int(cards[0]), int(cards[1])}, Олег - {int(cards[2]), int(cards[3])}')
+regole = np.random.choice(range(1, 30), size=2, replace=False)
+esercizii = np.random.choice(range(1, 6), size=2, replace=False)
+print(f'Дима: правила - {regole[0]}, упражнения - {esercizii[0]}; \nОлег: правила - {regole[1]}, упражнения - {esercizii[1]}')
